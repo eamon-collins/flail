7ed3cbf9e7a096f09cfb2b960dca20d8e40539fc
eamon-collins <eamonacollins@gmail.com>
2021-03-05 16:42:47 -0500

    graph axis needs fixing, ok for now

--- analyze/sentiment.py
+++ analyze/sentiment.py
@@ -4,15 +4,31 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import time
 from datetime import timedelta, datetime
 import matplotlib.pyplot as plt
+from matplotlib.dates import drange, DateFormatter
 import numpy as np
 from os import path
+import json
 
 from django.db import models, transaction, connection, IntegrityError
 from django.core.exceptions import ValidationError
 from fetch.models import Comment, Submission, SentimentRating, Ticker
-from fetch.reddit import load_tickers
+#from fetch.reddit import load_tickers
 from flail.settings import BASE_DIR
 from django.utils.timezone import make_aware
+
+#this method is duplicated here so it can execute within it's own thread
+def load_tickers(filepath=None):
+	with open(filepath) as file:
+		tickers = json.load(file)
+	#make sure we have these in database
+	for company, data in tickers.items():
+		newTicker, created = Ticker.objects.update_or_create(
+			ticker_symbol = data["ticker_symbol"],
+			keywords = json.dumps(data["keywords"])
+			#defaults = {"keywords":data["keywords"]}
+			)
+
+	return tickers
 
 #checks for new, unevaluated comments straight from the autist's fingers
 def analyze_queue():
@@ -75,13 +91,19 @@ def prepare_sentiment_graph(ticker):
 	x_positive = np.zeros(num_x_segments)
 	x_neutral = np.zeros(num_x_segments)
 	x_negative = np.zeros(num_x_segments)
-	x_axis = np.arange(num_x_segments)
+	
 	time_counter = now_time - delta
+	more_now_time = datetime.now()
+	#x_axis = drange(more_now_time-delta, more_now_time, x_time_segment)
+	x_axis = np.arange(num_x_segments)
+	print("start: "+time_counter.strftime('%d:%H:%M') + "  end: "+now_time.strftime('%d:%H:%M'))
+	#print("numxseg " + repr(num_x_segments)+ "lenxaxis "+repr(len(x_axis)))
+	index = 0
 	while not (time_counter >= now_time):
 		increment = current_days_posts.filter(created_utc__gte=time_counter, created_utc__lt=time_counter+x_time_segment)
-		increment = increment.order_by('-created_utc')
+		#increment = increment.order_by('-created_utc')
 
-		for index, sentimentRating in enumerate(increment):
+		for sentimentRating in increment:
 			if sentimentRating.sentiment_rating >= .2:
 				x_positive[index] += 1
 			elif sentimentRating.sentiment_rating <= -.2:
@@ -89,15 +111,23 @@ def prepare_sentiment_graph(ticker):
 			else:
 				x_neutral[index] += 1
 
+		index +=1
 		time_counter += x_time_segment
 
+	# print(x_axis)
+	# print(x_positive)
+	# print(x_negative)
+	#extend x axis
+	#plt.figure(figsize=(12,3))
+	#ax = plt.subplot(111)
+	fig, ax = plt.subplots(figsize=(12,3))
+	ax.xaxis_date(tz='EST')
+	ax.xaxis.set_major_formatter(DateFormatter('%H:%M'))
+	ax.bar(x_axis, x_positive, color= 'g', align='center')
+	ax.bar(x_axis, x_neutral, color= 'grey', bottom = x_positive, align='center')
+	ax.bar(x_axis, x_negative, color='r', bottom = x_positive+x_neutral, align='center')
 
-	plt.bar(x_axis, x_positive, color= 'g')
-	plt.bar(x_axis, x_neutral, color= 'grey', bottom = x_positive)
-	plt.bar(x_axis, x_negative, color='r', bottom = x_positive+x_neutral)
-
-	#plt.show()
-	print(path.join(BASE_DIR, 'static/graphs/'+repr(tickerObject.id)+'_sentiment.png'))
+	
 	plt.savefig(path.join(BASE_DIR, 'static/graphs/'+repr(tickerObject.id)+'_sentiment.png'))
 
 def prepare_all_graphs():
@@ -108,6 +138,7 @@ def prepare_all_graphs():
 
 		for company, data in TICKERS.items():
 			prepare_sentiment_graph(data)
+
 
 		#not sure if this is really needed, but probably at least 
 		#best to keep doing it until we get a non-file db solution up
