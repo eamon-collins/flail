1aca9a8fb6029bf3a8b414358d0bdce14d172b73
eamon-collins <eamonacollins@gmail.com>
2021-07-02 16:04:10 -0400

    still have errors

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     url='https://github.com/eamon-collins/flail',
 	install_requires=['django==3.0.5','django-crispy-forms==1.9.0','django-extensions==2.2.9',
                           'pandas','requests==2.23.0',
-                          'uwsgi==2.0.18', 'jupyter==1.0.0', 'ipython==7.14.0', 'psycopg2-binary',
+                          'uwsgi==2.0.18', 'jupyter==1.0.0', 'ipython==7.14.0', 'psycopg2-binary==2.8.6',
                           'html5lib==1.1', 'prettyprint==0.1.5', 'celery',
                           'response==0.4.0', 'flask==1.1.2', 'django-apscheduler', 'apscheduler',
                           'djangorestframework', 'SQLAlchemy', 'django-redis', 'django-celery-results',
